e4e30468fc754f3edad4128efaeaaf1359a7d933
Sara Victoria Lopez <slopez12@hawk.iit.edu>
2017-09-06 18:16:03 -0500

    added form and append

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 $('#user-form').on('submit', function(e) {
   var username = $('#username').val();
-  $('#results').append('<li><a href="https://api.github.com/users/' + username + '">' + username + '</a></li>');
+  $('#results').append('<li><a href="https://api.github.com/users/' + username + '">' +
+  username + '</a></li>');
   e.preventDefault();
 });
