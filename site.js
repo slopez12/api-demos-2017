ddb956f3fd70ca9057bca76823f7a782f13079ff
Sara Victoria Lopez <slopez12@hawk.iit.edu>
2017-09-11 18:11:36 -0500

    Added data and Google link

--- site.js
+++ site.js
@@ -1,6 +1,21 @@
+var user = {
+  "login": "slopez",
+  "id": 1366498,
+  "avatar_url": "https://avatars3.githubusercontent.com/u/1366498?v=4"
+}
+
+$('#user-form').append('<img src="' + user['avatar_url'] +'" />');
+
 $('#user-form').on('submit', function(e) {
   var username = $('#username').val();
   $('#results').append('<li><a href="https://api.github.com/users/' + username + '">' +
   username + '</a></li>');
   e.preventDefault();
+});
+$('#username').on('focus', function() {
+    console.log('The username element is focused. No "e"');
+  });
+$('#google').on('click', function(event) {
+  console.log('OMG they tired to go to Google!');
+  event.preventDefault();
 });
